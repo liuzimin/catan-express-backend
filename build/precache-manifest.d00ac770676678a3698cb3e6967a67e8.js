79eab0b7371716bb41280ce19bca718ba0949468
liuzimin <david9719717@uahoo.com>
2019-03-31 23:02:19 -0400

    added index

---
+++ build/precache-manifest.d00ac770676678a3698cb3e6967a67e8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fba7689dff4b69bd8dadf0b68d2b36c",
+    "url": "/static/media/rocks.0fba7689.png"
+  },
+  {
+    "revision": "ca5097518935b1c5889f",
+    "url": "/static/css/main.8f90b8fb.chunk.css"
+  },
+  {
+    "revision": "5eacd72b236772fe587637b0f6e7856a",
+    "url": "/static/media/yearofplenty.5eacd72b.jpg"
+  },
+  {
+    "revision": "8a3dfb187b9878bfa121",
+    "url": "/static/js/1.8a3dfb18.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb58de8885c9d2e89fe6512673578c6d",
+    "url": "/static/media/house.eb58de88.png"
+  },
+  {
+    "revision": "7073481d4a79044f71207ec3e35fcd81",
+    "url": "/static/media/wheat.7073481d.png"
+  },
+  {
+    "revision": "ca5097518935b1c5889f",
+    "url": "/static/js/main.ca509751.chunk.js"
+  },
+  {
+    "revision": "470a82f0758d3ee4c072fa8e607789df",
+    "url": "/static/media/development.470a82f0.png"
+  },
+  {
+    "revision": "6d7145c05be25e12974f6e5092417e02",
+    "url": "/static/media/natural_resources.6d7145c0.png"
+  },
+  {
+    "revision": "5a608c30d23ef6f43cdc1db430b3e6bd",
+    "url": "/static/media/VP_dev.5a608c30.jpg"
+  },
+  {
+    "revision": "d3c7c0c5815e1af2b1d66fefc210a3ea",
+    "url": "/static/media/monopoly.d3c7c0c5.jpg"
+  },
+  {
+    "revision": "37277f9ca9e4f04a836d196c7bd0a8e2",
+    "url": "/static/media/road_builders.37277f9c.jpg"
+  },
+  {
+    "revision": "8a3dfb187b9878bfa121",
+    "url": "/static/css/1.baa7d3ae.chunk.css"
+  },
+  {
+    "revision": "91dd16ed483be9d66bb05d73b81f05df",
+    "url": "/index.html"
+  }
+];
